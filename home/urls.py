5a5815bb38c4775552bec2b7a11b198aea5a06a4
yashasvinitripathi11 <yashasvinitripathi@gmail.com>
2023-09-01 00:25:39 +0530

    updated homepage

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
      path("", views.home, name="home"),
+     path("faq", views.faq, name="faq"),
+     path("contact", views.contact, name="contact"),
+     path("about", views.about, name="about"),
 ]
 
--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render,redirect
 def home(request):
      return render(request,"home/index.html")
 
+def faq(request):
+     return render(request,"home/faq.html")
+def contact(request):
+     return render(request,"home/contact.html")
+def about(request):
+     return render(request,"home/about.html")
+
+
+
+
+
+
